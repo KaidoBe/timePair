307ce87bffc65984e3ba03895b869c9b4841ed7b
Doryan Saint Georges <Doryan.saintgeorges@contraste.com>
2023-02-05 21:23:18 +0100

    adding common-api-lib

---
+++ api/common-api-lib/src/main/java/be/kaido/commonapilib/CommonApiLibApplication.java
@@ -0,0 +1,13 @@
+package be.kaido.commonapilib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApiLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonApiLibApplication.class, args);
+	}
+
+}
