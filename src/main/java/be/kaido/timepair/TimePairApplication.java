2fa472f33957ea78369f1a27efc33bac0e59bc13
Doryan Saint Georges <Doryan.saintgeorges@contraste.com>
2023-02-05 21:06:03 +0100

    initialize

---
+++ src/main/java/be/kaido/timepair/TimePairApplication.java
@@ -0,0 +1,13 @@
+package be.kaido.timepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimePairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimePairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/kaido/timepair/TimePairApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kaido.timepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimePairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
