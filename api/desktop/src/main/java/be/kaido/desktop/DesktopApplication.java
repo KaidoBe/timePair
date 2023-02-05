35b866902f317c739116997d932832e34d9ee435
Doryan Saint Georges <Doryan.saintgeorges@contraste.com>
2023-02-05 21:34:41 +0100

    adding mobile and desktop api

---
+++ api/desktop/src/main/java/be/kaido/desktop/DesktopApplication.java
@@ -0,0 +1,13 @@
+package be.kaido.desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesktopApplication.class, args);
+	}
+
+}
---
+++ api/desktop/src/test/java/be/kaido/desktop/DesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kaido.desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
